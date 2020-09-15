f96de08cab18c6b72af1bb78379235d5fb51901b
Kyle King <kylejking@gmail.com>
2020-09-15 00:21:33 -0400

    add icon textures

--- webpack/src/die.js
+++ webpack/src/die.js
@@ -1,78 +1,89 @@
+import $ from 'jquery';
 import * as THREE from 'three';
 import * as CANNON from 'cannon';
 import fontfaceonload from 'fontfaceonload';
 
-function gen_polyhedron(geometry) {
-  var vertices = [];
-  var faces = [];
-  var i = 0; 
-  var v = null; 
-  var f = null;
+const dieIcons = [
+  'running', 'running',
+  'user-injured', 'user-injured', 
+  'head-side-virus', 'head-side-virus'
+];
 
-  
-  for (i = 0; i < geometry.vertices.length; i++) {
-    v = geometry.vertices[i];
-    vertices[i] = new CANNON.Vec3(v.x, v.y, v.z);
-  }
+const iconMetadata = $.getJSON( "/fontawesome/metadata/icons.json");
 
-  for (i = 0; i < geometry.faces.length; i++) {
-    f = geometry.faces[i];
-    faces[i] = [f.a, f.b, f.c];
-  }
-
-  return new CANNON.ConvexPolyhedron(vertices, faces);
-}
-
-export default icons => {
+const dieMaterials = dieIcons.map(text => {
   const context = document.createElement('canvas').getContext('2d');
-
+  
   const canvas = context.canvas;
   [canvas.width, canvas.height] = [128, 128];
-  const texture = new THREE.CanvasTexture(canvas);
   
   context.fillStyle = "black";
   context.textAlign = "center";
-  context.font = '900 64px "Font Awesome 5 Free"';
-
-  // FontAwesome doesn't load until we try to make an icon but since
-  // it's not loaded yet, the icon will not appear  ¯\_(ツ)_/¯ 
-  context.fillText("\uf5dc", canvas.width/2, canvas.height/2 + 20);
   
+  // FontAwesome doesn't load until we try to make an icon but since
+  // it's not loaded yet, the icon will not appear  ¯\_(ツ)_/¯
+  context.globalAlpha = 0; 
+  context.font = '900 64px "Font Awesome 5 Free"';
+  context.fillText("?", canvas.width/2, canvas.height/2);
+
+  const texture = new THREE.CanvasTexture(canvas);
+
   // this fires when FontAwesome is ready, so we re-draw the icon onto the canvas
   fontfaceonload('Font Awesome 5 Free', { 
-    success: () => { 
-      // clear canvas
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      
-      // redraw 
-      context.fillStyle = "red";
-      context.fillText("\uf5dc", canvas.width/2, canvas.height/2 + 25);
-      
-      texture.needsUpdate = true;
+    success: () => {
+      iconMetadata.done(lookup => {
+        // get unicode character for icon
+        const unicode = lookup[text].unicode;
+        const charCode = String.fromCharCode(`0x${unicode}`);
+        
+        context.globalAlpha = 1;
+        context.fillText(charCode, canvas.width/2, canvas.height/2 + 25);
+        
+        // tell the renderer that this texture has changed
+        texture.needsUpdate = true;
+      });
     }
   });
 
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
-  var material = new THREE.MeshBasicMaterial( { map: texture } );
+  return new THREE.MeshBasicMaterial({map: texture, transparent: true});
+});
+
+const toShape = geometry => {
+  const vertices = geometry.vertices.map(vertex =>
+    new CANNON.Vec3(vertex.x, vertex.y, vertex.z)
+  );
+
+  const faces = geometry.faces.map(face =>
+    [face.a, face.b, face.c]
+  );
   
-  let die = new THREE.Mesh(geometry, material);
+  return new CANNON.ConvexPolyhedron(vertices, faces);
+}
+
+export default () => {
+  const geometry = new THREE.BoxGeometry(1,1,1);
+  
+  const die = new THREE.Mesh(
+    geometry, 
+    new THREE.MeshBasicMaterial({color: 'yellow'})
+  );
+  
+  die.add(new THREE.Mesh(geometry, dieMaterials));
   die.castShadow = true;
 
-  var edgeGeometry = new THREE.EdgesGeometry( die.geometry );
-  
-  var edgeMaterial = new THREE.LineBasicMaterial( { 
-    linewidth: 1,
-    color: 0xff0000
-  } );
-
-  var edges = new THREE.LineSegments( edgeGeometry, edgeMaterial );
-
-  die.add(edges);
+  die.add(
+    new THREE.LineSegments( 
+      new THREE.EdgesGeometry(die.geometry), 
+      new THREE.LineBasicMaterial({ 
+        linewidth: 1, color: 'darkgray'
+      })
+    )
+  );
 
   // move this to userdata
   die.body = new CANNON.Body({
     mass: 100,
-    shape: gen_polyhedron(geometry),
+    shape: toShape(geometry),
     material: new CANNON.Material({
       friction: 100,
       restitution: 1
